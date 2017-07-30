a7d4ccec9877d336d7904fa876b8c39797a12560
Julia Kim <jkim0620@gmail.com>
2017-07-30 17:13:19 -0400

    Remove class after animation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Content loaded!");
 
-  let form = document.getElementById("form");
-  let firstName = document.getElementById("first-name");
-  let openForm = document.getElementById("open-form");
+  let openFormBtn = document.getElementById("open-form");
   let modal = document.getElementById("modal");
   let sendBtn = document.getElementById("sendBtn");
 
-  openForm.addEventListener("click", () => {
+  openFormBtn.addEventListener("click", () => {
     modal.style.display = "block";
   });
 
@@ -17,15 +15,30 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("input", animateButton);
   }
 
-  // Function that adds a class that triggers animation
+  // Function that adds a class that triggers animation that indicates it's ready to continue
   const animateButton = () => {
     // CHECK IF every form fields has contents
-    if (messageForm.firstName.value && messageForm.lastName.value && messageForm.email.value && messageForm.message.value) {
+    if (messageForm.name.value && messageForm.email.value && messageForm.message.value) {
       // IF yes, add a class to the button that triggers an animation that let's the user know that button is ready to be clicked
       // alert("Form ready to submit");
       sendBtn.classList.add("enabeld");
     }
   }
+
+  // Function that adds a class that triggers animation that indicates that form is not ready to be submitted
+  const animateError = (e) => {
+    // IF form has empty field, add a class that animates accordingly
+    if (!messageForm.name.value || !messageForm.email.value || !messageForm.message.value) {
+      sendBtn.classList.add("refused");
+
+      // Remove animation 
+      setTimeout(function() {
+        sendBtn.classList.remove("refused");
+    }, 1000);
+    }
+  }
+
+  sendBtn.addEventListener("click", animateError);
 
   validateForm();
 
