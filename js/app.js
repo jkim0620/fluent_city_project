727626866fc380f0cab4ccd7215ed85cbbcfa4e3
Julia Kim <jkim0620@gmail.com>
2017-07-29 20:49:49 -0400

    Create seperate function for triggering animation for cleaner code

--- js/app.js
+++ js/app.js
@@ -13,20 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Function to validate if the form is ready to be submitted
   const validateForm = () => {
-    // Button starts as disabled mode
-    // WHEN form has empty field set button stays disabled
-
     // ON CHANGE of the form
-    form.addEventListener("input", () => {
-      // CHECK IF every form fields has contents
-      if (messageForm.firstName.value && messageForm.lastName.value && messageForm.email.value && messageForm.message.value) {
-        // IF yes, add a class to the button that triggers an animation that let's the user know that button is ready to be clicked
-        // alert("Form ready to submit");
-        sendBtn.classList.add("enabeld");
-      }
-    });
+    form.addEventListener("input", animateButton);
+  }
 
-    // AFTER form has been submittedset the button back to its original state
+  // Function that adds a class that triggers animation
+  const animateButton = () => {
+    // CHECK IF every form fields has contents
+    if (messageForm.firstName.value && messageForm.lastName.value && messageForm.email.value && messageForm.message.value) {
+      // IF yes, add a class to the button that triggers an animation that let's the user know that button is ready to be clicked
+      // alert("Form ready to submit");
+      sendBtn.classList.add("enabeld");
+    }
   }
 
   validateForm();
